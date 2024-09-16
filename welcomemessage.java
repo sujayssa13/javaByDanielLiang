08578f47f80d394e01eebbbd1e64c6e172204420
sujayssa13 <sujay.ssa13@gmail.com>
2024-09-16 20:11:29 +0530

    Create welcomemessage.java

---
+++ welcomemessage.java
@@ -0,0 +1,10 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+public class WelcomeWithThreeMessages{
+    public static void main (String[] args){
+        System.out.println("Programming is fun");
+        System.out.println("Fundamental first");
+        System.out.println("Problem driven");
+    }
+}
